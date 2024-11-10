db46acfae5e39e9fd09453a778f4a4804349c0c7
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-11-10 06:46:42 +0530

    First Commit

---
+++ Graphs/Toposort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+    vector<int> topologicalSort(vector<vector<int>>& adj) {
+        vector<int> indegree(adj.size(),0);
+        for(auto a: adj){
+            for(int c:  a){
+                indegree[c]++;
+            }
+        }
+        queue<int> qt;
+        for (int i = 0; i < indegree.size(); i++) {
+            if (indegree[i] == 0) qt.push(i);
+        }
+
+        vector<int> ans;
+        while(!qt.empty()){
+            int front = qt.front();
+            qt.pop();
+            ans.push_back(front);
+            for(int i: adj[front]){
+                indegree[i]--;
+                if(indegree[i]==0) qt.push(i);
+            }
+        }
+        return ans;
+        
+    }
