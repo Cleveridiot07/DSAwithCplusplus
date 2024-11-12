0afd1f73d628980591dfeff5b912ef3c70fc64df
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-11-12 06:01:56 +0530

    commit

---
+++ Graphs/ShortestPath/Djisktra's Algorithm/UsingQueue.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto arr: edges){
+            adj[arr[0]].push_back({arr[1],arr[2]});
+        }
+        
+        vector<int> Pathcost(V,-1);
+        queue<pair<int,int>> qt;
+        qt.push({0,0});
+        Pathcost[0] = 0;
+        while(!qt.empty()){
+            int current_weight = qt.front().second;
+            int current_node = qt.front().first;
+            qt.pop();
+            for(auto arr: adj[current_node]){
+                if(Pathcost[arr.first] == -1 || Pathcost[arr.first] > current_weight + arr.second ){
+                    Pathcost[arr.first] = current_weight + arr.second;
+                }else{
+                    continue;
+                }
+                qt.push({arr.first,current_weight + arr.second});
+            }
+        }
+        return Pathcost;
+    }
+};
