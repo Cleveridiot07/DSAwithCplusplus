7165df68c5d4ab98c56cc18c22b5500c589e7e7e
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-11-14 07:02:30 +0530

    Update

--- Graphs/Disjoint Set/DistjointSet(UnionFind).cpp
+++ Graphs/Disjoint Set/DistjointSet(UnionFind).cpp
@@ -7,6 +7,8 @@ using namespace std;
 class DisjointSet
 {
     vector<int> rank, uparent;
+
+public:
     DisjointSet(int n)
     {
         rank.resize(n + 1, 0);
@@ -15,36 +17,45 @@ class DisjointSet
             uparent[i] = i;
     }
 
-    bool isSameSet(int a,int b){
-        return uparent[a] == uparent [b];
+    bool isSameSet(int a, int b)
+    {
+        return uparent[a] == uparent[b];
     }
 
-    int findUparent(int node){
-        if(node == uparent[node]) return node;
-        return uparent[node] = findUparent(uparent[node]); 
+    int findUparent(int node)
+    {
+        if (node == uparent[node])
+            return node;
+        return uparent[node] = findUparent(uparent[node]);
     }
 
-    void UnionByRank(int node1, int node2){
+    void UnionByRank(int node1, int node2)
+    {
         int up1 = findUparent(node1);
         int up2 = findUparent(node2);
 
-        if(up1 == up2) return;
+        if (up1 == up2)
+            return;
 
-        if(rank[up1]<rank[up2]){
+        if (rank[up1] < rank[up2])
+        {
             uparent[up1] = up2;
-        }else if(rank[up1]>rank[up2]){
+        }
+        else if (rank[up1] > rank[up2])
+        {
             uparent[up2] = up1;
-        }else{
+        }
+        else
+        {
             uparent[up1] = up2;
             rank[up2]++;
         }
-
     }
-
 };
 
 int main()
 {
-    
+    DisjointSet ds(7);
+
     return 0;
 }
