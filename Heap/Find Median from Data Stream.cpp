6fcca9972a5b5ba40b9af70c7889a6a7d4ea8ee4
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-11-27 06:25:39 +0530

    Blind 75 Heaps P2

---
+++ Heap/Find Median from Data Stream.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+class MedianFinder {
+public:
+    std::priority_queue<int> maxHeap; 
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+
+    MedianFinder() {}
+
+    void addNum(int num) {
+        maxHeap.push(num);
+
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.top(); 
+        } else {
+            return (maxHeap.top() + minHeap.top()) / 2.0; 
+        }
+    }
+};
+
+
