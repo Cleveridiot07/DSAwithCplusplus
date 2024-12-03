ac8163bd385865486561ac275f9fad2e85414baf
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-12-03 08:12:03 +0530

    Commit

---
+++ Remove Element.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int i = 0;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+};
