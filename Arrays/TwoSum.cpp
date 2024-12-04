65d19c81d66d433e149830d3d2dd588eec44bcb1
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-12-04 05:52:42 +0530

    Added Two Sum

---
+++ Arrays/TwoSum.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; 
+    }
+};
--- index.cpp
+++ index.cpp
@@ -1,3 +1,10 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
 #include <iostream>
 using namespace std;
 int main(){
