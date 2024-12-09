346693c04ef462c54d72c3f555b8f5d7dc500fb0
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-12-09 06:54:28 +0530

    Update Set Matrix Zeroes

--- Arrays/Medium/Set Matrix Zeroes.cpp
+++ Arrays/Medium/Set Matrix Zeroes.cpp
@@ -7,42 +7,49 @@
 #include <string>
 #include <iostream>
 using namespace std;
+// Time Complexity = O(n*m)
+// Space Complexity = O(1)
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m= matrix.size();
-        int n= matrix[0].size();
-        bool column = true;
+        int m = matrix.size(), n = matrix[0].size();
+        bool firstRow = false, firstCol = false;
 
-        for(int i=0; i<m; i++){
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) firstCol = true;
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) firstRow = true;
+        }
 
-               if(matrix[i][0] == 0 ){
-                    column = false;
-                    
-                }
-            
-            for(int j=1; j<n; j++){
-
-             
-
-                if(matrix[i][j] == 0){
-                   matrix[i][0] = 0;
-                   matrix[0][j] = 0; 
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i=m-1; i>=0; i--){
-            for(int j=n-1; j>=1; j--){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
-                } 
+                }
             }
-            if(!column){
+        }
+
+        if (firstCol) {
+            for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
             }
         }
-       
+        if (firstRow) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
     }
 };
 int main(){
