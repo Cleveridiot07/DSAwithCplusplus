bec6ef94ba208e311137cf3ca2f302808c4dc16d
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-12-13 05:15:33 +0530

    Create Missing Number

---
+++ BinarySearch/Easy/Missing Number.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != i) return i;
+        }
+        return nums.size();
+        
+    }
+};
