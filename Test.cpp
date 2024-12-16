8614212bd9dce754a10c0c18acaec5510d08835b
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-12-16 21:32:00 +0530

    Revision

---
+++ Test.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void printv(vector<int> v){
+    for(int i:v) cout<<i<<" ";
+    cout<<endl;
+}
+
+int Knapsack(){
+
+
+
+    return 0;
+}
+
+int main(){
+    
+    cout<<"Hello"<<endl;
+}
