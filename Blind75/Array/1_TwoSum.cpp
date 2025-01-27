18e5d52ae005151828aacab4074d5d5b64b9c506
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-27 05:30:36 +0530

    Created TwoSum.cpp

---
+++ Blind75/Array/1_TwoSum.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <set>
+using namespace std;
+
+// Typedefs
+typedef long long ll;
+typedef double db;
+typedef unsigned int ui;
+typedef vector<ll> VT;
+typedef pair<int, int> pii;
+
+// Macros
+#define sz(x) ((int)((x).size()))
+#define max_val(a, b) (a < b ? b : a)
+#define min_val(a, b) (a < b ? a : b)
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+
+template <typename T>
+void printVector(vector<T> &arr) {
+    for (int i = 0; i < sz(arr); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+// Approach 1 O(n*logn)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int el  = nums[i];
+            if(mp.find(target-el) != mp.end()){
+                return {mp[target-el],i};
+            }
+            mp[el] = i;
+        }
+        return {-1,-1};
+    }
+};
+
+
+
+
+void clever() {
+    // write your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        clever();
+    }
+    return 0;
+}
