98c2787ab238e8d9bcd34a45d4a70962d36f378b
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-28 06:29:11 +0530

    q10

---
+++ Blind75/Array/10_Container With Most Water.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <set>
+using namespace std;
+
+// Typedefs
+typedef long long ll;
+typedef double db;
+typedef unsigned int ui;
+typedef vector<ll> VT;
+typedef pair<int, int> pii;
+
+// Macros
+#define sz(x) ((int)((x).size()))
+#define max_val(a, b) (a < b ? b : a)
+#define min_val(a, b) (a < b ? a : b)
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+
+template <typename T>
+void printVector(vector<T> &arr) {
+    for (int i = 0; i < sz(arr); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Approach 1 O(N)
+
+class Solution {
+public:
+    int maxArea(vector<int>& nums) {
+        int maxA = INT_MIN;
+        int i = 0, j = nums.size()-1;
+        while(i<j){
+            int Area = min(nums[i],nums[j])*(j-i);
+            maxA = max(maxA,Area);
+            if(nums[i]<nums[j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return maxA;        
+    }
+};
+
+void clever() {
+    // write your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        clever();
+    }
+    return 0;
+}
