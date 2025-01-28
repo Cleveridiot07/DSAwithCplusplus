5ff1f2100be825bd9428bbacf54f623cdf582d11
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-28 07:23:42 +0530

    b-q4

---
+++ Blind75/Binary/4_Missing Number.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <set>
+using namespace std;
+
+// Typedefs
+typedef long long ll;
+typedef double db;
+typedef unsigned int ui;
+typedef vector<ll> VT;
+typedef pair<int, int> pii;
+
+// Macros
+#define sz(x) ((int)((x).size()))
+#define max_val(a, b) (a < b ? b : a)
+#define min_val(a, b) (a < b ? a : b)
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+
+template <typename T>
+void printVector(vector<T> &arr) {
+    for (int i = 0; i < sz(arr); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Approach O(n)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+};
+
+
+void clever() {
+    // write your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        clever();
+    }
+    return 0;
+}
