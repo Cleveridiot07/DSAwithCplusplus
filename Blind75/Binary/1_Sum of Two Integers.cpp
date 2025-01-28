17ccc6fd4e40edccce71b969576ef4028b4dfa81
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-28 06:50:36 +0530

    b-q1

---
+++ Blind75/Binary/1_Sum of Two Integers.cpp
@@ -0,0 +1,70 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <set>
+using namespace std;
+
+// Typedefs
+typedef long long ll;
+typedef double db;
+typedef unsigned int ui;
+typedef vector<ll> VT;
+typedef pair<int, int> pii;
+
+// Macros
+#define sz(x) ((int)((x).size()))
+#define max_val(a, b) (a < b ? b : a)
+#define min_val(a, b) (a < b ? a : b)
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+
+template <typename T>
+void printVector(vector<T> &arr) {
+    for (int i = 0; i < sz(arr); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        // Loop until there is no carry
+        while (b != 0) {
+            // Calculate the carry, which is common set bits of a and b, shifted left by one
+            int carry = (a & b) << 1;
+            // Sum of bits of a and b where at least one of the bits is not set
+            a = a ^ b;
+            // Assign carry to b, so it can be added in the next iteration
+            b = carry;
+        }
+        // When carry is zero, a contains the sum
+        return a;
+    }
+};
+
+void clever() {
+    // write your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        clever();
+    }
+    return 0;
+}
