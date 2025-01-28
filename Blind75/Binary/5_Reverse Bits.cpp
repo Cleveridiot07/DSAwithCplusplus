4076cc6f96a9c3f8b80d71fb529d98be5e1f7bf9
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-28 07:34:02 +0530

    b-q5

---
+++ Blind75/Binary/5_Reverse Bits.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <set>
+using namespace std;
+
+// Typedefs
+typedef long long ll;
+typedef double db;
+typedef unsigned int ui;
+typedef vector<ll> VT;
+typedef pair<int, int> pii;
+
+// Macros
+#define sz(x) ((int)((x).size()))
+#define max_val(a, b) (a < b ? b : a)
+#define min_val(a, b) (a < b ? a : b)
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+
+template <typename T>
+void printVector(vector<T> &arr) {
+    for (int i = 0; i < sz(arr); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+// Approach O(n)
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        int i = 0;
+        
+        while (n > 0) {
+            int el = n & 1;
+            ans = (ans << 1) | el;
+            n >>= 1;
+            i++; 
+        }
+
+        while (i < 32) {
+            ans <<= 1;
+            i++;
+        }
+
+        return ans;    
+    }
+};
+
+
+void clever() {
+    // write your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        clever();
+    }
+    return 0;
+}
