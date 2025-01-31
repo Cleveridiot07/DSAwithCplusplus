259574997aca2b51052c80a70a0b6b869992c237
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-31 07:14:54 +0530

    T-q7

---
+++ Blind75/Trees/7_Subtree of Another Tree.cpp
@@ -0,0 +1,85 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <set>
+using namespace std;
+
+// Typedefs
+typedef long long ll;
+typedef double db;
+typedef unsigned int ui;
+typedef vector<ll> VT;
+typedef pair<int, int> pii;
+
+// Macros
+#define sz(x) ((int)((x).size()))
+#define max_val(a, b) (a < b ? b : a)
+#define min_val(a, b) (a < b ? a : b)
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+
+template <typename T>
+void printVector(vector<T> &arr) {
+    for (int i = 0; i < sz(arr); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Recursive Solution 
+// TC : O(n^2)
+class Solution {
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
+        if(p->val != q->val) return false;
+        bool isLeft = isSameTree(p->left,q->left);
+        bool isRight = isSameTree(p->right,q->right);
+        return isLeft && isRight;
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root ==  nullptr) return false;
+        bool check = false;
+        if(root->val == subRoot->val){
+            check = isSameTree(root,subRoot);
+        }
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot) || check;
+    }
+};   
+
+void clever() {
+    // write your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        clever();
+    }
+    return 0;
+}
