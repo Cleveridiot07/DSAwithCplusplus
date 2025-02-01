f4ed68d98cfe0fe0b4ce6d5bd22065f7485fdde3
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-01 06:26:43 +0530

    L-q2

---
+++ Blind75/LinkedList/2_Detect Cycle.cpp
@@ -0,0 +1,82 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <set>
+using namespace std;
+
+// Typedefs
+typedef long long ll;
+typedef double db;
+typedef unsigned int ui;
+typedef vector<ll> VT;
+typedef pair<int, int> pii;
+
+// Macros
+#define sz(x) ((int)((x).size()))
+#define max_val(a, b) (a < b ? b : a)
+#define min_val(a, b) (a < b ? a : b)
+#define cy cout << "YES" << endl;
+#define cn cout << "NO" << endl;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define fi first
+#define se second
+
+template <typename T>
+void printVector(vector<T> &arr) {
+    for (int i = 0; i < sz(arr); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// O(n)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast){
+            fast = fast->next;
+            if(fast){
+                fast = fast->next;
+                slow = slow->next;
+            }
+            if(fast == slow) return true;
+        }
+
+        return false;
+    }
+};
+
+void clever() {
+    // write your code here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        clever();
+    }
+    return 0;
+}
