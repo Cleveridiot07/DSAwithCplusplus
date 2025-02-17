c6f2aff1f3a340c5bff1d7b12a3cd38f95c83b73
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-17 15:31:25 +0530

    Time: 18 ms (78.89%) | Memory: 149.2 MB (30.67%) - LeetSync

--- LeetcodeSolutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ LeetcodeSolutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,41 +1,23 @@
 class NumMatrix {
+    vector<vector<int>> prefix;
 public:
-    vector<vector<int>> arr;
     NumMatrix(vector<vector<int>>& matrix) {
-        // first we calculate the sum row wise
-        arr = matrix;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        for(int i = 0 ; i< n ; i++){
-            for(int j = 1 ; j<m;j++){
-                arr[i][j] += arr[i][j-1];
-            }
-        }
-        // now similary colulating the column wise sum 
-        for(int j = 0;j<m; j++){
-            for(int i = 1; i< n ; i++){
-                arr[i][j] += arr[i-1][j];
+        int m = matrix.size(), n = matrix[0].size();
+        prefix = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefix[i][j] = matrix[i - 1][j - 1] + 
+                               prefix[i - 1][j] + 
+                               prefix[i][j - 1] - 
+                               prefix[i - 1][j - 1];
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        // now implementing the formula
-        int ans = arr[row2][col2];
-        if (row1 > 0) {
-            ans -= arr[row1 - 1][col2];
-        }
-        if (col1 > 0) {
-            ans -= arr[row2][col1 - 1];
-        }
-        if (row1 > 0 && col1 > 0) {
-            ans += arr[row1 - 1][col1 - 1];
-        }
-        return ans;
+        row1++, col1++, row2++, col2++;
+        return prefix[row2][col2] - prefix[row1 - 1][col2] - 
+               prefix[row2][col1 - 1] + prefix[row1 - 1][col1 - 1];
     }
 };
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
