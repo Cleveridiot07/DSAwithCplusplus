ecfbff5c421c17b3a9b574ed92625898729e1934
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-18 10:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.82%) - LeetSync

--- LeetcodeSolutions/69-sqrtx/sqrtx.cpp
+++ LeetcodeSolutions/69-sqrtx/sqrtx.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int mySqrt(int x) {
         int s = 0 , e = x;
-        int sqr = 1;
+        int sqr = 0;
         while( s <= e){
             long long int mid = s + (e-s)/2;
             if(mid*mid == x) return mid;
