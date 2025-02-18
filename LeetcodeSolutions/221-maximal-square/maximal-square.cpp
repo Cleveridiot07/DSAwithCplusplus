7c2dd75b5452d49513c5369138db96307b707d57
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-18 15:20:12 +0530

    Time: 12 ms (39.52%) | Memory: 31.1 MB (15.06%) - LeetSync

--- LeetcodeSolutions/221-maximal-square/maximal-square.cpp
+++ LeetcodeSolutions/221-maximal-square/maximal-square.cpp
@@ -14,7 +14,7 @@ public:
             for (int j = 0; j < cols; ++j) {
                 if (matrix[i][j] == '1') {
                     if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
+                        dp[i][j] = matrix[i][j] - '0';
                     } else {
                         dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
                     }
