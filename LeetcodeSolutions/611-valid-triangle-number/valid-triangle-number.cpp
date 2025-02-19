6303d0083eb7daf53bac29edd4959b6971a3e4a2
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-19 14:46:56 +0530

    Time: 31 ms (74.04%) | Memory: 16.7 MB (9.24%) - LeetSync

--- LeetcodeSolutions/611-valid-triangle-number/valid-triangle-number.cpp
+++ LeetcodeSolutions/611-valid-triangle-number/valid-triangle-number.cpp
@@ -2,16 +2,26 @@ class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int index = 0;
-        int ans = 0;
-        while(index < nums.size() && nums[index] == 0  ) index++;
-        for(int i=index;i<nums.size();i++){
-            for(int j = i+1;j<nums.size()-1;j++){
-                auto it = lower_bound(nums.begin(),nums.end(),nums[i] + nums[j]) - (nums.begin() + j);
-                ans += it-1;
-            }
-        }
+        int count=0;
+        for(int i=nums.size()-1;i>=0;i--){
+            int j=0;
+            int k=i-1;
+            while(j<k){
 
-        return ans;
+                if(nums[j]+nums[k]>nums[i]){
+                    count+=k-j;
+                    k--;
+                    
+                }else{
+                    j++;
+                }
+                
+                
+            }}
+            return count;
+
+        
+        
+
     }
 };
