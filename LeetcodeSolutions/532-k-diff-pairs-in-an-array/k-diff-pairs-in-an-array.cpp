87db89a59355d84520ff0f3f1e3a9c6d4d2f64ee
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-19 11:06:04 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (94.70%) - LeetSync

--- LeetcodeSolutions/532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ LeetcodeSolutions/532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        int i = 0, j = 1;
+        int i = 0, j = 0;
         int count = 0;
         while(i <= j && j < nums.size()){
             int curr = abs(nums[j] - nums[i]);
