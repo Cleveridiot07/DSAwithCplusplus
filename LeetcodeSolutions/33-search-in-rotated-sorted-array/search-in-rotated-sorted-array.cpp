298bcc17183e724c68d2b8844e04851ff7d8a3d4
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-19 10:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (94.77%) - LeetSync

--- LeetcodeSolutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ LeetcodeSolutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int s = 0, e = nums.size()-1;
         while(s <= e){
             int mid = s + (e-s)/2;
-            if(nums[mid]==target) return mid;
+            if(nums[mid] == target) return mid;
             if(nums[s] <= nums[mid]){
                 if(nums[s] <= target && nums[mid] > target) e = mid-1;
                 else s = mid+1;
