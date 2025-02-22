293a582551566db72af6cae7faeea8c38b086747
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-22 15:28:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.02%) - LeetSync

--- LeetcodeSolutions/874-backspace-string-compare/backspace-string-compare.cpp
+++ LeetcodeSolutions/874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> st1;
-        stack<char> st2;
-        for(auto ch: s){
-            if(ch != '#') st1.push(ch);
-            else if (st1.size()  > 0) st1.pop();
-        }
-        for(auto ch: t){
-            if(ch != '#') st2.push(ch);
-            else if (st2.size()  > 0) st2.pop();
-        }
-        while(st1.size()>0 || st2.size()>0){
-            if(st1.size() == 0 || st2.size() == 0) return false;
-            if(st1.top() != st2.top()) return false;
-            st1.pop();
-            st2.pop();
+        int skip1 = 0, skip2 = 0;
+        int i = s.size() - 1, j = t.size() - 1;
+
+        while (i >= 0 || j >= 0) {
+
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skip1++, i--;
+                } else if (skip1 > 0) {
+                    skip1--, i--;
+                } else {
+                    break;
+                }
+            }
+
+   
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skip2++, j--;
+                } else if (skip2 > 0) {
+                    skip2--, j--;
+                } else {
+                    break;
+                }
+            }
+
+            if (i >= 0 && j >= 0 && s[i] != t[j]) {
+                return false;
+            }
+
+            if ((i >= 0) != (j >= 0)) {
+                return false;
+            }
+
+            i--, j--;
         }
 
         return true;
