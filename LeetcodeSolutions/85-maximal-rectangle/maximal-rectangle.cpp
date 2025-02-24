3ac58389b2deac037bd96ffdbea72a11e27b37f3
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-24 11:23:02 +0530

    Time: 10 ms (45.59%) | Memory: 17.7 MB (80.59%) - LeetSync

--- LeetcodeSolutions/85-maximal-rectangle/maximal-rectangle.cpp
+++ LeetcodeSolutions/85-maximal-rectangle/maximal-rectangle.cpp
@@ -2,25 +2,36 @@ class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         if (matrix.empty()) return 0;
+        
         int m = matrix.size(), n = matrix[0].size();
-        vector<vector<pair<int, int>>> dp(m, vector<pair<int, int>>(n, {0, 0}));
+        vector<int> heights(n, 0);  
         int maxArea = 0;
+        auto calculateMaxArea = [](const vector<int>& heights) {
+            stack<int> s;
+            int maxArea = 0;
+            int n = heights.size();
+            
+            for (int i = 0; i <= n; i++) {
+                int h = (i == n) ? 0 : heights[i];
+                while (!s.empty() && h < heights[s.top()]) {
+                    int height = heights[s.top()];
+                    s.pop();
+                    int width = s.empty() ? i : i - s.top() - 1;
+                    maxArea = max(maxArea, height * width);
+                }
+                s.push(i);
+            }
+            return maxArea;
+        };
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    int length = (j > 0) ? dp[i][j - 1].first + 1 : 1;
-                    int breadth = (i > 0) ? dp[i - 1][j].second + 1 : 1;
-                    dp[i][j] = {length, breadth};
-
-                    int minWidth = length;
-                    for (int k = 0; k < breadth; k++) {
-                        minWidth = min(minWidth, dp[i - k][j].first);
-                        maxArea = max(maxArea, minWidth * (k + 1));
-                    }
-                }
+                heights[j] = (matrix[i][j] == '1') ? heights[j] + 1 : 0;
             }
+            
+            maxArea = max(maxArea, calculateMaxArea(heights));
         }
+        
         return maxArea;
     }
 };
