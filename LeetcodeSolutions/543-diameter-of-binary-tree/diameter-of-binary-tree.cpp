316ae1da581a0945603aa025104757a25c9aabd7
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-25 11:05:49 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (42.82%) - LeetSync

---
+++ LeetcodeSolutions/543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int diameter(TreeNode* root, int &maxD) {
+        if (root == nullptr) return 0;
+
+        int left = diameter(root->left, maxD);
+        int right = diameter(root->right, maxD);
+        
+        maxD = max(maxD, left + right);
+        return max(left, right) + 1;
+    }
+
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxD = 0;
+        diameter(root, maxD);
+        return maxD;
+    }
+};
