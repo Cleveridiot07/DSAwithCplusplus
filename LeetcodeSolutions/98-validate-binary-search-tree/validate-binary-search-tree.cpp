65ad5b30178c645650c99be5b9be072cb4771332
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-26 10:10:49 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (76.03%) - LeetSync

---
+++ LeetcodeSolutions/98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+private:
+    bool helper(TreeNode* root,long long int lRange,long long int RRange){
+        if (!root) return true;
+        if(!(root->val > lRange && root->val<RRange)) return false;
+        return helper(root->left,lRange,root->val) && helper(root->right,root->val,RRange);
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        return helper(root,(1L*INT_MIN)-1,(1L*INT_MAX)+1);
+    }
+};
