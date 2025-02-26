c6911dbae95f0354e37570287a9d28e74e15d8b1
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-02-26 11:08:29 +0530

    Time: 2414 ms (5.02%) | Memory: 8 MB (60.99%) - LeetSync

--- LeetcodeSolutions/96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ LeetcodeSolutions/96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,11 +1,12 @@
 class Solution {
-    int numberOfWays(int n){
+    int numberOfWays(int n,vector<int> &dp){
         if(n == 0 || n == 1) return 1;  
 
+        if(dp[n] != -1) return dp[n];
         int number = 0;
         for(int i = 1; i <= n; i++){
-            int left = numberOfWays(i - 1);  
-            int right = numberOfWays(n - i); 
+            int left = numberOfWays(i - 1,dp);  
+            int right = numberOfWays(n - i,dp); 
 
             number += (left * right); 
         }
@@ -14,6 +15,7 @@ class Solution {
     }
 public:
     int numTrees(int n) {
-        return numberOfWays(n);
+        vector<int> dp(n+1,-1);
+        return numberOfWays(n,dp);
     }
 };
