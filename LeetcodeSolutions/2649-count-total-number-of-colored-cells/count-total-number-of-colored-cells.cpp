71ccbcd9c128f0a35a5d67f704d9f3a0a2e779a4
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-03-05 07:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (7.10%) - LeetSync

--- LeetcodeSolutions/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ LeetcodeSolutions/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,10 +1,6 @@
 class Solution {
-    long long helper(long long n){
-        if(n <= 1) return n;
-        else return (n-1)*4 + helper(n-1);
-    }
 public:
     long long coloredCells(int n) {
-        return helper(n);
+        return 1LL * n * n + 1LL * (n - 1) * (n - 1);
     }
 };
